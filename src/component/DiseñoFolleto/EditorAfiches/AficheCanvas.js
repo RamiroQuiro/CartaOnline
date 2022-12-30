d3137b772556e0523ba2c526d923456648bfdf07
ramiroquiro <rama.exe.13@gmail.com>
2022-12-30 14:20:17 -0300

    actualizando canvaQR

--- src/component/DiseñoFolleto/EditorAfiches/AficheCanvas.js
+++ src/component/DiseñoFolleto/EditorAfiches/AficheCanvas.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useOutletContext } from "react-router-dom";
 import qrcode from "qrcode";
 import Canvas from "./Canvas";
@@ -7,7 +7,10 @@ import jsPDF from "jspdf";
 
 export default function AficheCanvas({}) {
   const [imagen, setImagen] = useState(null);
-  const [printer, setPrinter] = useState(false)
+  const [printer, setPrinter] = useState(false);
+  const canvasRef = useRef(null);
+  const canvas = canvasRef.current;
+  const ctx = canvas?.getContext("2d");
   const { perfilCuenta } = useOutletContext();
   const formatoQR = {
     color: { light: "#ffffeeff", dark: "#00001Eff" },
@@ -33,51 +36,47 @@ export default function AficheCanvas({}) {
     imagenesArray();
   }, []);
 
-const imprimirCanvas=()=>{
-
-var can =document.getElementById('myCanvas')
-console.log(can)
-var ctx=can.getContext("2d")
-var imagen=can.toDataURL();
-console.log(imagen)
-var link=document.createElement('a')
-link.href=imagen;
-link.download="lklkjl.png"
-link.click()
-
-
-  // setPrinter(true)
-// console.log(printer)
-}
-const handleDownloadPDF = () => {
-  const input = document.getElementById('myCanvas')
-  html2canvas(input, { logging: true, useCORS: true }).then((canvas) => {
-    const imgWidth = 14.3;
-    const imgHeight = (canvas.height * imgWidth) / canvas.width;
-    const imgData = canvas.toDataURL();
-    const pdf = new jsPDF("portrait", "cm", "a5");
-    pdf.addImage(imgData, "JPG", 0, 0, imgWidth, imgHeight);
-    pdf.save("qrCarta-Online.pdf");
-  });
-};
+  const downloadImage = () => {
+    const link = document.createElement("a");
+    link.download = "qrCarta-Online.jpg";
+    link.href = canvas.toDataURL();
+    link.click();
+  };
+  const handleDownloadPDF = () => {
+    const input = document.getElementById("myCanvas");
+    html2canvas(input, { logging: true, useCORS: true }).then((canvas) => {
+      const imgWidth = 14.3;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      const imgData = canvas.toDataURL();
+      const pdf = new jsPDF("portrait", "cm", "a5");
+      pdf.addImage(imgData, "JPG", 0, 0, imgWidth, imgHeight);
+      pdf.save("qrCarta-Online.pdf");
+    });
+  };
   return (
     <div className="w-full">
       <Canvas
-      printer={printer}
-      setPrinter={setPrinter}
-      id="myCanvas"
-      businessName={perfilCuenta?.perfilUser?.businessName}
+        businessName={perfilCuenta?.perfilUser?.businessName}
         width={"720"}
         qrCode={qrCode}
         height="960"
         className=" relative   flex flex-col items-center justify-between mx-auto  overflow-hidden"
         logo={imagen?.find((img) => img?.posicion == "logo")?.url}
+        canvas={canvas}
+        canvasRef={canvasRef}
+        ctx={ctx}
       />
       <button
-      onClick={imprimirCanvas}
+        onClick={downloadImage}
         className="uppercase font-medium text-xs w-full md:w-1/4 border text-white bg-blue-400 rounded hover:bg-white hover:border-blue-400 duration-500 hover:text-blue-400 px-3 py-2"
       >
-        Imprimir
+        download
+      </button>
+      <button
+        onClick={handleDownloadPDF}
+        className="uppercase font-medium text-xs w-full md:w-1/4 border text-white bg-blue-400 rounded hover:bg-white hover:border-blue-400 duration-500 hover:text-blue-400 px-3 py-2"
+      >
+        downloadPDF
       </button>
     </div>
   );
